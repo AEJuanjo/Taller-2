2f6c8447582512d065eea5fb961efa4a6155a835
Luis_2006 <rodriguezmosqueraluisleonardo@gmail.com>
2026-02-25 09:03:42 -0500

    Inicializacion 5 variables

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        
+         int edad = 18;
+        double estatura = 1.70;
+        float peso = 65f;
+        short codigoPostal = 050001;
+        int poblacionCiudad = 1500000;
+    }
+}
