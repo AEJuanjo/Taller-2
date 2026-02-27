7b5c4b6f2160448b3ec7a006208052c471de2e74
Juan José Alzate <juanjosealzateescudero@gmail.com>
2026-02-26 19:23:57 -0500

    Panel para escoger ecuación

--- Ecuaciones.java
+++ Ecuaciones.java
@@ -1,1 +1,15 @@
 import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;
+
+public class Ecuaciones {
+
+    public static void main(String[] args) {
+
+        int opcion = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Seleccione la ecuación:\n"
+                        + "1. Ecuación 1\n"
+                        + "2. Ecuación 2"));
+
+    }
+}
