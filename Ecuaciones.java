8003d5ec0162d65d82fa12c02ec7871c5b13679a
Juan José Alzate <juanjosealzateescudero@gmail.com>
2026-02-26 19:27:54 -0500

    Pedir valor de Y
    Added input for variable z in the Ecuaciones class.

--- Ecuaciones.java
+++ Ecuaciones.java
@@ -6,13 +6,15 @@ public class Ecuaciones {
     public static void main(String[] args) {
 
         int opcion = Integer.parseInt(
-                JOptionPane.showInputDialog(
+            JOptionPane.showInputDialog(
                         "Seleccione la ecuación:\n"
                         + "1. Ecuación 1\n"
                         + "2. Ecuación 2"));
-                double x = Double.parseDouble(
+            double x = Double.parseDouble(
                 JOptionPane.showInputDialog("Ingrese el valor de x:"));
-
+            double z = Double.parseDouble(
+                JOptionPane.showInputDialog("Ingrese el valor de z:"));
     }
 }
+
 
