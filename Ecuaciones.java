4188b369158a72cfbbaa137e959c2c4c675fd7f1
Juan José Alzate <juanjosealzateescudero@gmail.com>
2026-02-26 19:31:01 -0500

    Condicional para ecuacion 1
    Added logic for equation calculation based on user selection.

--- Ecuaciones.java
+++ Ecuaciones.java
@@ -15,8 +15,14 @@ public class Ecuaciones {
             double z = Double.parseDouble(
                 JOptionPane.showInputDialog("Ingrese el valor de z:"));
             double y = 0;
+        if (opcion == 1) {
+
+            y = (((3*x/(1+((3*z*z+2)/(3*x)))))/1) / ((1/(1+z)) + 3*x*x + 2*z + 3);
+
+        } 
     }
 }
+
 
 
 
