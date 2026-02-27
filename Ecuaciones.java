25b7a05a76a2f6eaeccd6f53cb594c77d71de8e9
Juan José Alzate <juanjosealzateescudero@gmail.com>
2026-02-26 19:25:58 -0500

    Ingreso de variable x
    Added input for variable x after equation selection.

--- Ecuaciones.java
+++ Ecuaciones.java
@@ -10,6 +10,9 @@ public class Ecuaciones {
                         "Seleccione la ecuación:\n"
                         + "1. Ecuación 1\n"
                         + "2. Ecuación 2"));
+                double x = Double.parseDouble(
+                JOptionPane.showInputDialog("Ingrese el valor de x:"));
 
     }
 }
+
